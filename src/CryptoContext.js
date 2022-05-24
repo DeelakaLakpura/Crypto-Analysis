d986fba15f643412c77ad8b8bcdbdcbc745b356e
deelaka <deelaka.lakpura94@gmail.com>
2022-05-24 18:27:09 +0530

    First

---
+++ src/CryptoContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const Crypto = createContext();
+
+const CryptoContext = ({ children }) => {
+  const [currency, setCurrency] = useState("LKR");
+  const [symbol, setSymbol] = useState("රු");
+
+  useEffect(() => {
+    if (currency === "LKR") setSymbol("රු");
+    else if (currency === "USD") setSymbol("$");
+  }, [currency]);
+
+  return (
+    <Crypto.Provider value={{ currency, setCurrency, symbol }}>
+      {children}
+    </Crypto.Provider>
+  );
+};
+
+export default CryptoContext;
+
+export const CryptoState = () => {
+  return useContext(Crypto);
+};
